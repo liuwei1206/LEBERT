1a624925747bb9502ea92feb908d03034d2a8f6a
liuwei1206 <34615810+liuwei1206@users.noreply.github.com>
2021-06-16 10:03:37 +0800

    Update and rename run_ner.sh to run_demo.sh
    rewrite the shell

--- run_demo.sh
+++ run_demo.sh
@@ -1,19 +1,19 @@
 CUDA_VISIBLE_DEVICES=5 python3 -m torch.distributed.launch --master_port 13517 --nproc_per_node=1 \
        Trainer.py --do_train --do_eval --do_predict --evaluate_during_training \
-                  --data_dir="data/dataset/NER/msra" \
-                  --output_dir="data/result/NER/msra/lebertcrf" \
+                  --data_dir="data/dataset/CWS/demo" \
+                  --output_dir="data/result/CWS/demo/lebertcrf" \
                   --config_name="data/berts/bert/config.json" \
                   --model_name_or_path="data/berts/bert/pytorch_model.bin" \
                   --vocab_file="data/berts/bert/vocab.txt" \
                   --word_vocab_file="data/vocab/tencent_vocab.txt" \
                   --max_scan_num=1000000 \
                   --max_word_num=5 \
-                  --label_file="data/dataset/NER/msra/labels.txt" \
+                  --label_file="data/dataset/CWS/demo/labels.txt" \
                   --word_embedding="data/embedding/word_embedding.txt" \
-                  --saved_embedding_dir="data/dataset/NER/msra" \
-                  --model_type="LEBertCRF_Token" \
+                  --saved_embedding_dir="data/dataset/CWS/demo" \
+                  --model_type="WCBertCRF_Token" \
                   --seed=106524 \
-                  --per_gpu_train_batch_size=16 \
+                  --per_gpu_train_batch_size=4 \
                   --per_gpu_eval_batch_size=16 \
                   --learning_rate=1e-5 \
                   --max_steps=-1 \
@@ -21,4 +21,4 @@ CUDA_VISIBLE_DEVICES=5 python3 -m torch.distributed.launch --master_port 13517 -
                   --num_train_epochs=20 \
                   --warmup_steps=190 \
                   --save_steps=600 \
-                  --logging_steps=600
+                  --logging_steps=100
